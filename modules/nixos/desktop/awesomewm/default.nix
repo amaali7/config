db412e0eb414d4e77abae24774a389d9ce126fad
Abdallah Ali <amaali1991@gmail.com>
2024-01-24 14:18:06 +0200

    add AwesomeWM

---
+++ modules/nixos/desktop/awesomewm/default.nix
@@ -0,0 +1,46 @@
+{ options, config, lib, pkgs, ... }:
+with lib; with lib.amaali7;
+let
+  cfg = config.amaali7.desktop.awesomewm;
+in
+{
+  options.amaali7.desktop.awesomewm = with types; {
+    enable = mkBoolOpt false "Whether or not to enable Awesome Window Manager.";
+  };
+
+  config = mkIf cfg.enable {
+    services.acpid = enabled;
+    services.xserver = {
+      enable = true;
+      displayManager = {
+        defaultSession = "none+awesome";
+      };
+      windowManager.awesome = {
+        enable = true;
+        package = pkgs.awesome-git;
+        luaModules = with pkgs.luaPackages; [
+          luarocks # is the package manager for Lua modules
+          luadbi-mysql # Database abstraction layer
+        ];
+      };
+    };
+    amaali7 = {
+      desktop = {
+        common-tiling = enabled;
+        themes = enabled;
+        xfce = enabled;
+      };
+      home = {
+        extraOptions = {
+          home.packages = with pkgs; [
+            foot
+            rofi
+            imagemagick
+            sassc
+          ];
+        };
+      };
+    };
+
+  };
+}
