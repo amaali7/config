33413c0946777a4cecd367dbbf94e9db801643db
Abdallah Ali <amaali1991@gmail.com>
2024-02-08 23:16:50 +0200

    fix fonts

--- modules/nixos/system/fonts/default.nix
+++ modules/nixos/system/fonts/default.nix
@@ -46,17 +46,18 @@ in
       ] ++ cfg.fonts;
 
       # use fonts specified by user rather than default ones
-      enableDefaultPackages = false;
+      # enableDefaultPackages = false;
 
       # user defined fonts
       # the reason there's Noto Color Emoji everywhere is to override DejaVu's
       # B&W emojis that would sometimes show instead of some Color emojis
-      fontconfig.defaultFonts = {
-        serif = [ "Noto Serif" "Noto Color Emoji" ];
-        sansSerif = [ "Noto Sans" "Noto Color Emoji" ];
-        monospace = [ "JetBrainsMono Nerd Font" "Noto Color Emoji" ];
-        emoji = [ "Noto Color Emoji" ];
-      };
+      # fontconfig.defaultFonts = {
+      #   serif = [ "Noto Serif" "Noto Color Emoji" ];
+      #   sansSerif = [ "Noto Sans" "Noto Color Emoji" ];
+      #   monospace = [ "JetBrainsMono Nerd Font" "Noto Color Emoji" ];
+      #   emoji = [ "Noto Color Emoji" ];
+      # };
+      fontDir = enabled;
     };
 
   };
