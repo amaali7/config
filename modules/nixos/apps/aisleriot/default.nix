ec251343e7e3e8b3c8af84e3a34a062bbdbeb3d4
Abdallah Ali <amaali1991@gmail.com>
2024-02-22 20:48:43 +0200

    more game

---
+++ modules/nixos/apps/aisleriot/default.nix
@@ -0,0 +1,18 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.amaali7;
+let
+  cfg = config.amaali7.apps.aisleriot;
+in
+{
+  options.amaali7.apps.aisleriot = with types; {
+    enable = mkBoolOpt false "Whether or not to enable doukutsu-rs.";
+  };
+
+  config = mkIf cfg.enable {
+    environment.systemPackages = with pkgs.amaali7; [
+gnome.aisleriot
+    ];
+  };
+}
